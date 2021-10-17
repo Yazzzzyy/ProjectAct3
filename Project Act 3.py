35e61fed49e6dbcda23c76af2dcd056bd0451672
raeannemalaya-iics <61110848+raeannemalaya-iics@users.noreply.github.com>
2021-10-18 02:29:17 +0800

    Update Project Act 3.py

--- Project Act 3.py
+++ Project Act 3.py
@@ -11,3 +11,15 @@ print("Region: ", get('https://ipapi.co/region/').text)
 print ("Country: ", get('https://ipapi.co/country_name/').text)
 
 print("Top level Domain: ", get('https://ipapi.co/country_tld/').text)
+
+print("Timezone: ", get('https://ipapi.co/timezone/').text)
+
+print("Currency: ", get('https://ipapi.co/currency/').text)
+
+print("Currency Name: ", get('https://ipapi.co/currency_name/').text)
+
+print("Language: ", get('https://ipapi.co/languages/').text)
+
+print("Autonomous System Number: ", get('https://ipapi.co/asn/').text)
+
+print("Organization: ", get('https://ipapi.co/org/').text)
