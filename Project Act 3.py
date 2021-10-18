156030d161b8bad06821192018b5474c3d142c64
asamm123 <60645800+asamm123@users.noreply.github.com>
2021-10-18 15:16:32 +0800

    Update Project Act 3.py

--- Project Act 3.py
+++ Project Act 3.py
@@ -1,25 +1,24 @@
 from requests import get
 
-print("The information regarding your IP address is shown below")
+print("The information regarding your IP address is shown below") 
 
-print ("IP Address: ", get('https://ipapi.co/ip/').text)
+print ("IP Address: ", get('https://ipapi.co/ip/').text) #Gets the IP address
 
-print ("City: ", get('https://ipapi.co/city/').text)
+print ("City: ", get('https://ipapi.co/city/').text) #Gets the City
 
-print("Region: ", get('https://ipapi.co/region/').text)
+print("Region: ", get('https://ipapi.co/region/').text) #Gets the Region
 
-print ("Country: ", get('https://ipapi.co/country_name/').text)
+print ("Country: ", get('https://ipapi.co/country_name/').text) #Gets the Country
 
-print("Top level Domain: ", get('https://ipapi.co/country_tld/').text)
+print("Top level Domain: ", get('https://ipapi.co/country_tld/').text) #Gets the Top level Domain
 
+print("Currency: ", get('https://ipapi.co/currency/').text) #Gets the Currency
 
-print("Currency: ", get('https://ipapi.co/currency/').text)
+print("Currency Name: ", get('https://ipapi.co/currency_name/').text) #Gets the Currency Name
 
-print("Currency Name: ", get('https://ipapi.co/currency_name/').text)
+print("Language: ", get('https://ipapi.co/languages/').text)  #Gets the Language
 
-print("Language: ", get('https://ipapi.co/languages/').text)
-
-print("Autonomous System Number: ", get('https://ipapi.co/asn/').text)
+print("Autonomous System Number: ", get('https://ipapi.co/asn/').text) #Gets the Autonomous Systen Number
 
 print("Organization: ", get('https://ipapi.co/org/').text)
 
